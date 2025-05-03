9e9b729d9b17de2ef2a61b92225e89cd60afa0c2
Omprakash939 <139845783+Omprakash939@users.noreply.github.com>
2025-05-03 15:48:12 +0530

    change url

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "development" ? "/" : "/portfolio/om-prakash/",
+  base: mode === "development" ? "/" : "/portfolio/",
 }));
